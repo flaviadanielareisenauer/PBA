564d33e87104b8feda3f3b41a81de15ede187698
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2024-05-01 11:42:13 -0300

    clase Recta

--- src/G1Ejercicio3/Recta.java
+++ src/G1Ejercicio3/Recta.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package G1Ejercicio3;
 
-/**
- *
- * @author Flavia
- */
 public class Recta {
-    
+
+    Punto p1, p2;
+
+    public Recta(Punto p1, Punto p2) {
+	this.p1 = p1;
+	this.p2 = p2;
+    }
+
+    public Recta() {
+	p1 = new Punto(1, 1);
+	p2 = new Punto(2, 2);
+    }
+
+    public float pendiente() {
+	return (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+    }
+
+    public boolean paralelas(Recta otraRecta) {
+	return pendiente() == otraRecta.pendiente();
+    }
 }
